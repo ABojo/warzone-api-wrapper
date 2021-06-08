236b0681a67b811d6d580128fd64a7d90f9c7a4d
ABojo <alex.bojorquez2598@gmail.com>
2021-06-08 03:38:07 -0400

    Added a requireLogin wrapper function that will take in a function as an arguement and then execute the function only if the isLoggedIn property on the session object equals true otherwise it will throw an error telling the user to first login before trying to use a method that requires auth

--- CoDAPI.js
+++ CoDAPI.js
@@ -10,6 +10,18 @@ const CoDAPI = () => {
     },
   };
 
+  const requireLogin = (cb) => {
+    return function (platform, username) {
+      if (session.isLoggedIn === true) {
+        return cb(platform, username);
+      } else {
+        throw new Error(
+          'You must call the .login() method with a valid activision email/password before making this request!'
+        );
+      }
+    };
+  };
+
   const login = async (username, password) => {
     try {
       const csrf = await loginHelpers.getCSRF();
@@ -28,34 +40,49 @@ const CoDAPI = () => {
     return session;
   };
 
-  const searchPlayer = async (platform, username) => {
+  const searchPlayer = requireLogin(async (platform, username) => {
     const response = await axios(
       `https://my.callofduty.com/api/papi-client/crm/cod/v2/platform/${platform}/username/${username}/search`,
       { headers: session.headers }
     );
 
     return response.data;
-  };
+  });
 
-  const getStats = async (platform, username) => {
+  const getStats = requireLogin(async (platform, username) => {
     const response = await axios(
       `https://my.callofduty.com/api/papi-client/stats/cod/v1/title/mw/platform/${platform}/gamer/${username}/profile/type/wz
     `,
       { headers: session.headers }
     );
     return response.data;
-  };
+  });
 
-  const getMatches = async (platform, username) => {
+  const getMatches = requireLogin(async (platform, username) => {
     const response = await axios(
       `https://my.callofduty.com/api/papi-client/crm/cod/v2/title/mw/platform/${platform}/gamer/${username}/matches/wz/start/0/end/0/details`,
       { headers: session.headers }
     );
 
     return response.data;
+  });
+
+  const getMatchDetails = async (id) => {
+    const response =
+      await axios(`https://www.callofduty.com/api/papi-client/crm/cod/v2/title/mw/platform/battle/fullMatch/wz/${id}/it
+    `);
+
+    return response.data;
   };
 
-  return { login, getSessionInfo, searchPlayer, getStats, getMatches };
+  return {
+    login,
+    getSessionInfo,
+    searchPlayer,
+    getStats,
+    getMatches,
+    getMatchDetails,
+  };
 };
 
 module.exports = CoDAPI;
